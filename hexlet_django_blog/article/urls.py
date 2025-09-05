d97b476b700ca93b91226fc71b535e2a0a8d6286
WhiteA <crazy032@mail.ru>
2025-09-05 18:57:22 +0300

    Просмотр (CRUD)

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from hexlet_django_blog.article.views import IndexView
+from hexlet_django_blog.article.views import IndexView, ArticleView
 
 
 app_name = "articles"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("<int:id>/", ArticleView.as_view(), name="show"),
 ]
