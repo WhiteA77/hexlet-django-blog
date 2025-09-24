52f4dad8719581b0f7ee97f7f66a814436c4ff30
WhiteA <crazy032@mail.ru>
2025-09-24 14:53:53 +0300

    Создание (CRUD)

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from hexlet_django_blog.article.views import IndexView, ArticleView
+from hexlet_django_blog.article.views import (
+    IndexView,
+    ArticleView,
+    ArticleFormCreateView,
+)
 
 
 app_name = "articles"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("create/", ArticleFormCreateView.as_view(), name="create"),
     path("<int:id>/", ArticleView.as_view(), name="show"),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
 from hexlet_django_blog.article.models import Article
+from hexlet_django_blog.article.forms import ArticleForm
 
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        articles = Article.objects.all()[:15]
+        articles = Article.objects.order_by("-created_at")[:15]
         return render(
             request,
             "articles/index.html",
@@ -25,4 +27,28 @@ class ArticleView(View):
             context={
                 "article": article,
             },
+        )
+
+
+class ArticleFormCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(
+            request,
+            "articles/create.html",
+            context={"form": form},
+        )
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            messages.success(request, "Статья успешно создана")
+            return redirect("articles:show", id=article.id)
+
+        messages.error(request, "Исправьте ошибки формы")
+        return render(
+            request,
+            "articles/create.html",
+            context={"form": form},
         )
